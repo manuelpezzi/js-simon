eb0322463b98a3d4e1e56708c695a47fcd5e6f24
manuelpezzi <pezzimanuel@gmail.com>
2025-01-21 12:42:06 +0100

    const create

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const coountdownEl = document.getElementById("countdown");
+const instructionsEl = document.getElementById('instructions');
+const numberListEl = document.getElementById('nuber-list');
+const answerFormEl = document.getElementById('answers-form');
+const imputGroupEl = document.getElementById('imput-group');
+const messageEl = document.getElementById('message');
