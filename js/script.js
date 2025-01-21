078fde4c94d8441079995b82b44e1a95b8e206cb
manuelpezzi <pezzimanuel@gmail.com>
2025-01-21 13:08:11 +0100

    fucntion per vericare i numeri inseriti dall utente

--- js/script.js
+++ js/script.js
@@ -38,3 +38,28 @@ function showAnswerForm() {
     instructionsEl.textContent = 'inserisci i numeri che ricordi:';
     answerFormEl.classList.remove('d-none');
 }
+function checkAnswers(event) {
+    event.preventDefault();
+    messageEl.textContent = '';
+
+    const userInputs = [];
+    const inputs = inputGroupEl.querySelectorAll('input');
+    for (let i = 0; i < inputs.length; i++) {
+        const value = parseInt(inputs[i].value, 10);
+        if (!isNaN(value)) {
+            userInputs.push(value);
+        }
+    }
+    if (new Set(userInputs).size !== userInputs.length) {
+        messageEl.textContent = 'Hai inserito numeri duplicati. Riprova!';
+        return;
+    }
+    const correctNumbers = [];
+    for (let i = 0; i < userInputs.length; i++) {
+        if (randomNumbers.includes(userInputs[i])) {
+            correctNumbers.push(userInputs[i]);
+        }
+
+    }
+    messageEl.textContent = `Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers.join(', ')}`;
+}
