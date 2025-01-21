3ced63cb767d9f1f301586c43bad196e279e754f
manuelpezzi <pezzimanuel@gmail.com>
2025-01-21 12:57:31 +0100

    fucntion countdown create

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ const numberListEl = document.getElementById('nuber-list');
 const answerFormEl = document.getElementById('answers-form');
 const imputGroupEl = document.getElementById('imput-group');
 const messageEl = document.getElementById('message');
+
+let randomNumbers = [];
+let countdown = 5;
+
+function generateRandomNumbers(count, min, max) {
+    const numbers = [];
+    for (let i = 0; i < count; i++) {
+        let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numbers.includes(randomNum)) {
+            numbers.push(randomNum)
+
+        } else {
+            i--;
+
+        }
+    }
+    return numbers;
+}
+function startCountdown() {
+    const timer = setInterval(function () {
+        coountdownEl.textContent = countdown;
+        countdown--;
+        if (countdown < 0) {
+            clearInterval(timer);
+            showAnswerForm();
+        }
+    }, 1000)
+}
